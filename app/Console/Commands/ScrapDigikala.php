8357e491bbec1ccab4f3e067e0f60e40eb0b65d7
Abilogos <dev.abi.log@gmail.com>
2021-11-27 00:45:52 +0330

    add command

---
+++ app/Console/Commands/ScrapDigikala.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ScrapDigikala extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrap:digikala {page}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $toPage = (int)$this->argument('page');
+        for ($i=1;$i<=$toPage;$i++) {
+            $url = "https://www.digikala.com/treasure-hunt/products/?pageno=${i}&sortby=1";
+        }
+        //$response = Http::get();
+        //dd($response);
+        return Command::SUCCESS;
+    }
+}
